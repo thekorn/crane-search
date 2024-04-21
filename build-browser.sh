a1e0681c82aaaf3e9a41456de693218c816a2cff
Markus Korn <markus.korn@gmail.com>
2024-04-21 12:43:33 +0200

    experiments with tinygo

--- build-browser.sh
+++ build-browser.sh
@@ -1,2 +1,3 @@
 mkdir -p dist
 GOOS=js GOARCH=wasm go build -o dist/crane.wasm browser/browser.go
+docker run --rm -v $(pwd):/src -w /src tinygo/tinygo tinygo build -no-debug -o ./dist/cranetiny.wasm -target=wasm ./browser/browser.go
